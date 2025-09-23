e44d1bf64241de9e6d70c0fa9c6f913ddf967928
Leônidas Araújo <leonidasaraujomqm@gmail.com>
2025-09-23 12:14:27 -0300

    add section headers for categories in Home component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,8 +6,11 @@ function Home() {
   console.log(mocks);
   return (
     <div className="home-container">
+      <h2>Animais</h2>
       <CardsList items={mocks.animais} />
+      <h2>Livros</h2>
       <CardsList items={mocks.livros} />
+      <h2>Pessoas</h2>
       <CardsList items={mocks.pessoas} />
     </div>
   );
